e157a455e8eca13c13c4224fdacf1df8fabe429c
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-12 14:57:31 +1100

    Add sumAverageElements

--- average_value_array/Program.cs
+++ average_value_array/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace average_value_array
 {
     class Program
     {
+        public void sumAverageElements(int[] arr, int size)
+        {
+            int sum = 0;
+            int average = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += arr[i];
+            }
+            average = sum / size;
+            Console.WriteLine("Sum Of Array is : " + sum);
+            Console.WriteLine("Averge Of Array is : " + average);
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
